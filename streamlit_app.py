1ee62140bb44897ce88eb3dabe5fe02f13985265
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2024-11-06 11:27:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,9 @@ st.write('**Raw data**')
 st.info('This is my first Machine Learning App on Streamlit!')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
+st.write('**x**')
+x = df.drop('species',axis=1)
+x
+st.write(**y**)
+y = df.species
+y
