707349ae8530e213832e668513af78fea3e99f15
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2024-11-06 11:19:22 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 st.title('🎈 Machine Learning App')
 
+With st.expander('Data'):
 st.info('This is my first Machine Learning App on Streamlit!')
 df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
 df
