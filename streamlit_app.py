aad09e94924427a1a763e4904521703846331c3f
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2024-11-08 11:26:50 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,6 @@ import pandas as pd
 st.title('🎈 Machine Learning App')
 st.info('This is my first Machine Learning App on Streamlit!')
 
-
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
   df
   st.write('**x**')
