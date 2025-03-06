2066042304114f6e6f8f4b847ab7049793896be5
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2025-03-06 14:48:19 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,3 @@ import pandas as pd
 st.title('🎈 Machine Learning App')
 st.info('This is my first Machine Learning App on Streamlit!')
 
-  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
-  df
-  st.write('**x**')
-  x = df.drop('species',axis=1)
-  x
-  st.write(**y**)
-  y = df.species
-  y
