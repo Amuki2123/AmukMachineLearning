aebb7691fc3466cdbe9040964265ed9de47b3aea
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2025-03-28 14:41:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,6 +73,7 @@ def train_neuralprophet(data):
         learning_rate=0.01,
         trend_reg=0,
         trainer_config={
+            'accelerator': 'auto',
             'max_epochs': 50,
             'enable_progress_bar': True
         }
