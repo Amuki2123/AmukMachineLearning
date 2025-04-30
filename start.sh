42db11e98617c215d9f206dc828192b4a3fcd571
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2025-04-30 18:47:26 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-streamlit run app.py \
+streamlit run your_app.py --server.port=8501 --logger.level=debug \
   --server.port=8501 \
   --server.fileWatcherType=none \
   --server.headless=true \
   --browser.gatherUsageStats=false
+  
